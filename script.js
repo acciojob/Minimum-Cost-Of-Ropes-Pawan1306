eaf9d80327f1e87d995d2f2d79846d3d34815b0d
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-25 19:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 function calculateMinCost() {
-  //your code here
+   
+            const ropeLengthsInput = document.getElementById('rope-lengths');
+            const resultElement = document.getElementById('result');
+            const inputText = ropeLengthsInput.value;
+            const cost = connectRopes(inputText);
+            resultElement.innerText = `Minimum cost of connecting ropes: ${cost}`;
+        }
+
+        function connectRopes(input) {
+            const ropes = input.split(',').map(Number);
+            
+            const minHeap = new MinHeap();
+            
+            for (const rope of ropes) {
+                minHeap.insert(rope);
+            }
+            
+            let totalCost = 0;
+            
+            while (minHeap.size() > 1) {
+                const rope1 = minHeap.extractMin();
+                const rope2 = minHeap.extractMin();
+                
+                const cost = rope1 + rope2;
+                totalCost += cost;
+                
+                minHeap.insert(cost);
+            }
+            
+            return totalCost;
+        }
+
+        class MinHeap {
+            // (Same MinHeap class as provided in the previous response)
+        }
   
   
   
-}  
+ 
